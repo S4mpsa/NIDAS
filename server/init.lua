d6feb898ba8c9136c2c7a577bca0fa2332580902
Gabriel Moreira Minossi <gordominossi@gmail.com>
2021-06-27 20:00:40 -0300

    Adding network communication on addMachine Adding listener for waypoint coordinates relative to tablet Adding listener for tablet coordinates Adding listener for machine name Moving config file rewriting code to a function

--- server/init.lua
+++ server/init.lua
@@ -9,18 +9,17 @@ local getMultiblockStatus = require("server.usecases.get-multiblock-status")
 
 --
 
-local machineList = addresses
-local function addToMachineList(_, address, _)
+local function updateMachineList(_, address, _)
     local comp = Component.proxy(address)
     if comp.type == "waypoint" or comp.type == "gt_machine" or comp.type == "gt_batterybuffer" then
-        machineList = addMachine(address, addressesConfigFile)
+        addMachine(address, addressesConfigFile)
     end
 end
-Event.listen("component_added", addToMachineList)
+Event.listen("component_added", updateMachineList)
 
 local statuses = {}
 while true do
-    for address, name in pairs(machineList) do
+    for address, name in pairs(addresses) do
         statuses[address] = getMultiblockStatus(address, name)
     end
     os.sleep(0)
