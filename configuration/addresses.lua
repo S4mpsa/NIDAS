d9fd1bc2fcaa3c57494e22209132c7c94e87ab9c
Gabriel Moreira Minossi <gordominossi@gmail.com>
2021-07-02 16:49:10 -0300

    Updating addresses format and constants

--- configuration/addresses.lua
+++ configuration/addresses.lua
@@ -1,6 +1,10 @@
 local addresses = {
-    ["1b05ef68-0fa7-4f94-818d-3e8c079e299d"] = "EBF 11",
-    ["753f5619-7076-45c0-8f0a-f3899bae00e1"] = "Cleanroom"
+    ["1b05ef68-0fa7-4f94-818d-3e8c079e299d"] = {
+        name = "EBF11"
+    },
+    ["753f5619-7076-45c0-8f0a-f3899bae00e1"] = {
+        name = "Cleanroom"
+    }
 }
 
 return addresses
--- configuration/constants.lua
+++ configuration/constants.lua
@@ -1,10 +1,9 @@
 local constants = {
     machineAddPort = 0xADD,
-    inputTimeout = 20, -- seconds
-    noInputTimeout = 2, -- seconds
-    tabletBootUpTime = 5, -- seconds
-    tabletResponseTime = 1, -- seconds
-    addressesConfigFile = "/home/NIDAS/configuration/addresses.lua"
+    inputTimeout = 10, -- seconds
+    tabletBootUpTime = 4, -- seconds
+    tabletResponseTime = 3, -- seconds
+    addressesConfigFile = "configuration.addresses"
 }
 
 return constants
