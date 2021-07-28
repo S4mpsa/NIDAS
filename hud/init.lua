8845608e9d4e0992b0018fd3ecd33fefdc93d4e0
Sampsa <sampo.vanninen@aalto.fi>
2021-07-28 17:02:11 +0300

    Add multi-glass to init

--- hud/init.lua
+++ hud/init.lua
@@ -7,14 +7,15 @@ local component = require("component")
 --
 
 local sampsaGlasses = component.proxy(component.get("af55dfbf"))
-
+local gordoGlasses = component.proxy(component.get("27f3251b"))
 local hud = {}
 
 ar.clear(sampsaGlasses)
+ar.clear(gordoGlasses)
 
 function hud.update(serverInfo)
-    powerDisplay.widget({{sampsaGlasses}}, serverInfo.power)
-    toolbar.widget({{sampsaGlasses, _, _, 3}})
+    powerDisplay.widget({{sampsaGlasses}, {gordoGlasses, {1920, 1080}}}, serverInfo.power)
+    toolbar.widget({{sampsaGlasses, _, _, 3}, {gordoGlasses, {1920, 1080}, _, -4}})
 end
 
 return hud
