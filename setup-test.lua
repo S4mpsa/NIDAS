eb27bf82e035151d0cd5d4ca9b820c4cd3e88ef9
Gabriel Minossi <gabriel.minossi@gmail.com>
2021-07-30 15:06:30 -0300

    Changing shell calls in setup-test to lua filesystem method calls

--- setup-test.lua
+++ setup-test.lua
@@ -1,10 +1,12 @@
 -- Paste this into OpenComputer terminal to download and set up NIDAS
 -- wget https://raw.githubusercontent.com/S4mpsa/NIDAS/develop/setup-test.lua -f
-
 local shell = require("shell")
+local filesystem = require("filesystem")
 
-local tarMan = "https://raw.githubusercontent.com/mpmxyz/ocprograms/master/usr/man/tar.man"
-local tarBin = "https://raw.githubusercontent.com/mpmxyz/ocprograms/master/home/bin/tar.lua"
+local tarMan =
+    "https://raw.githubusercontent.com/mpmxyz/ocprograms/master/usr/man/tar.man"
+local tarBin =
+    "https://raw.githubusercontent.com/mpmxyz/ocprograms/master/home/bin/tar.lua"
 
 shell.setWorkingDirectory("/usr/man")
 shell.execute("wget -fq " .. tarMan)
@@ -13,26 +15,21 @@ shell.execute("wget -fq " .. tarBin)
 
 NIDAS = "https://github.com/S4mpsa/NIDAS/releases/download/v-1/NIDAS.tar"
 
-local successfull =
-    pcall(
-    function()
-        shell.execute("rm -rf /home/NIDAS")
-        shell.execute("mkdir /home/NIDAS")
+local successfull = pcall(function()
+    filesystem.remove("/home/NIDAS")
+    filesystem.makeDirectory("/home/NIDAS")
 
-        shell.setWorkingDirectory("/home/NIDAS")
-        print("Downloading NIDAS")
-        shell.execute("wget -fq " .. NIDAS .. " -f")
-        print("Extracting")
-        shell.execute("tar -xf NIDAS.tar")
-        shell.execute("rm -f NIDAS.tar")
-        shell.execute("rm -rf /home/lib")
-        shell.execute("cp -r lib /home/lib")
-        shell.execute("cp -f .shrc /home/.shrc")
-        shell.execute("cp -f setup-test.lua /home/setup-test.lua")
+    shell.setWorkingDirectory("/home/NIDAS")
+    print("Downloading NIDAS")
+    shell.execute("wget -fq " .. NIDAS .. " -f")
+    print("Extracting")
+    shell.execute("tar -xf NIDAS.tar")
+    filesystem.remove("NIDAS.tar")
+    filesystem.remove("/home/lib")
+    shell.execute("cp -r lib /home/lib")
+    filesystem.copy(".shrc", "/home/.shrc")
+    filesystem.copy("setup-test.lua", "/home/setup-test.lua")
 
-        print("Success!\n")
-    end
-)
-if (not successfull) then
-    print("Update failed")
-end
+    print("Success!\n")
+end)
+if (not successfull) then print("Update failed") end
