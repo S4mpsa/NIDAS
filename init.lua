73ee4dc5d30feb34170f0f34ba0f21a3a9b63954
Dyonovan <Dyonovan@users.noreply.github.com>
2021-08-05 21:46:49 -0400

    Added ability to enable/disable modules

--- init.lua
+++ init.lua
@@ -3,13 +3,14 @@ local server = require("server")
 local hud = require("hud")
 local event = require("event")
 local powerControl = require("modules.tools.powerControl")
+local moduleSelection = require("configuration.modules")
 --
 
 local serverInfo
 
 while true do
     serverInfo = server.update()
-    hud.update(serverInfo)
-    powerControl(serverInfo.power, "b8583fd9")
+    if moduleSelection.hud then hud.update(serverInfo) end
+    if moduleSelection.powerControl then powerControl(serverInfo.power, "b8583fd9") end
     os.sleep()
 end
