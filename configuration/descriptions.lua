ce3b54096d040294af48e7de99dbc80f2c637d59
Sampsa <sampo.vanninen@aalto.fi>
2021-08-15 15:45:04 +0300

    Moved descriptions to their own file

---
+++ configuration/descriptions.lua
@@ -0,0 +1,7 @@
+local descriptions = {
+    hud = "Overlays a HUD on your screen. Each AR Glass can be configured independantly. X and Y resolutions are the rendering resolution of the game window. Scale is GUI Scale: 1 = Small, 2 = Normal, 3 = Large, 4-10 = Auto",
+    server = "Updates all data and handles communication between other servers. Required for most modules to work.",
+    powerControl = "Emits redstone when power levels are below a certain amount."
+}
+
+return descriptions
