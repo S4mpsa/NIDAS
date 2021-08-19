0880099f6ec8f97ded56eb06a4bb1b3791b5f892
Gabriel Minossi <gabriel.minossi@gmail.com>
2021-08-19 11:28:51 -0300

    Checking if the `system` is a `robot` and then checking for the navigation upgrade

--- init.lua
+++ init.lua
@@ -1,12 +1,23 @@
 package.path = package.path .. ";/home/NIDAS/?.lua;/home/NIDAS/lib/?.lua"
-local drone = false
-for _, name in pairs(require("component").list()) do
-    if name == "navigation" then
-        drone = true
-        require("drone")
+local robot = false
+for _, part in pairs(require("computer").getDeviceInfo()) do
+    if part.class == "system" and part.description == "Robot" then
+        robot = true
+        local navigation = false
+        for _, component in pairs(require("component").list()) do
+            if component == "navigation" then
+                navigation = true
+                require("robot")
+                break
+            end
+        end
+        if not navigation then
+            print("Navigation upgrade not installed!")
+            print("Please reassemble your robot.")
+        end
         break
     end
 end
-if not drone then
+if not robot then
     require("configuration")
 end
