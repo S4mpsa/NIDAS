71e09f955fde292f1320544774eec89e3ded15e8
Gabriel Minossi <gabriel.minossi@gmail.com>
2021-09-03 16:19:00 -0300

    Adding `findMatchingPattern` usecase

---
+++ modules/infusion/find-matching-pattern.lua
@@ -0,0 +1,54 @@
+-- Import section
+
+local component = require("component")
+
+--
+
+-- TODO: Get interface from getMachine
+local function exec()
+    local itemsInChest = {}
+
+    -- Adds all items in the chest connected through the storage bus to the list
+    for item in component.me_interface.allItems() do
+        if item.size > 0 then
+            itemsInChest[item.label] = item.size
+        end
+    end
+
+    -- Runs if there's an item in the chest
+    if #itemsInChest > 0 then
+        local maxInterfaceSlots = 36
+
+        -- Searches each slot of the interface for a matching pattern
+        local slot = 1
+        local pattern = component.me_interface.getInterfacePattern(slot)
+        local patternFound = false
+        while slot <= maxInterfaceSlots do
+            -- Checks all patterns inputs
+            patternFound = pattern and true
+            for _, input in ipairs(pattern and pattern.inputs or {}) do
+                -- Searches for a matching item in the chest for the pattern input
+                if input.name then
+                    local inputMatch = false
+                    for name, amount in pairs(itemsInChest) do
+                        if input.name == name and input.count == amount then
+                            inputMatch = true
+                            break
+                        end
+                    end
+
+                    patternFound = patternFound and inputMatch
+                end
+            end
+
+            if patternFound then
+                return pattern
+            end
+
+            pattern = component.me_interface.getInterfacePattern(slot)
+            slot = slot + 1
+        end
+    end
+end
+
+return exec
