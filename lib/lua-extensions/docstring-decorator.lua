268cdda009f335b8b9fcf92173169c7f58812ae3
gordominossi <gabriel.minossi@flashapp.com.br>
2022-06-30 21:44:43 -0300

    feat(lua-extensions): add docstring decorator

---
+++ lib/lua-extensions/docstring-decorator.lua
@@ -0,0 +1,20 @@
+local Decorator = require('lib.lua-extensions.decorator')
+
+
+--- Makes the function.__tostring = description.
+---
+--- Use by concatenating the docstring with the
+--- function to be described:
+---
+---     local myPrint =
+---       docstring([[calls .__tostring on whatever arguments it's given]]) ..
+---       function(args) print(args) end
+---@type decorator (description:string):decorator string: string to describe the function being decorated
+local docstring = Decorator(function(description, functionToDescribe)
+    return setmetatable({}, {
+        __call = function(_, ...) functionToDescribe(...) end,
+        __tostring = function() return description end
+    })
+end)
+
+return docstring
