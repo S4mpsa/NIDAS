7dab87fbb2aa9ea96cdf594f4d95a70c8b857b9d
gordominossi <gabriel.minossi@flashapp.com.br>
2022-07-08 01:06:49 -0300

    feat(core/tile-entity): add tile entity related models and types

---
+++ core/tile-entity/inventory.model.lua
@@ -0,0 +1,43 @@
+local component = require("component")
+local tileEntity = require("core.tile-entity")
+
+---@class inventory: tileEntity
+---@field name string
+---@field size number
+local inventory = {}
+
+---Creates a new inventory object
+---@param address string
+---@param location coordinates
+---@return inventory
+function inventory.new(address, location)
+    ---@type inventory
+    local self = tileEntity.new(address, location)
+
+    local proxy = component.proxy(address)
+    self.name = proxy.getInventoryName()
+    self.size = proxy.getInventorySize()
+
+    self.compareStacks = proxy.compareStacks
+    self.transferStack = proxy.transferStack
+
+    function self.getStackInSlot(slot)
+        ---@type itemStack
+        local stack = proxy.getStackInSlot(slot)
+        stack.maxStackSize = proxy.getSlotMaxStackSize(slot)
+    end
+
+    local content = {}
+    local function getContent()
+        content = proxy.getAllStacks()
+        for i in ipairs(content) do
+            content[i].maxStackSize = proxy.getSlotMaxStackSize(i)
+        end
+    end
+
+    function self.update() return {content = getContent()} end
+
+    return self
+end
+
+return inventory
