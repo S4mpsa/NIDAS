4c4d4887b7364a77f008ff0555e13cf275ffb904
gordominossi <gabriel.minossi@flashapp.com.br>
2022-07-17 11:42:59 -0300

    fix(inventory, transposer): use controllerSide on `inventory_controller`

--- core/tile-entity/inventory.lua
+++ core/tile-entity/inventory.lua
@@ -1,37 +1,70 @@
 local component = require("component")
 local tileEntity = require("core.tile-entity")
 
+local relativeSidePositions = {
+    [0] = 'below',
+    'above',
+    'north of',
+    'south of',
+    'west of',
+    'east of'
+}
+
 ---@class inventory: tileEntity
 ---@field name string
 ---@field size number
-local inventory = {}
+local inventory = {componentType = 'inventory_controller'}
 
 ---Creates a new inventory object
 ---@param address string
 ---@param location coordinates
+---@param controllerSide number index 0
 ---@return inventory
-function inventory.new(address, location)
+function inventory.new(address, location, controllerSide)
     ---@type inventory
     local self = tileEntity.bind(address, location)
 
     local proxy = component.proxy(address)
-    self.name = proxy.getInventoryName()
-    self.size = proxy.getInventorySize()
 
-    self.compareStacks = proxy.compareStacks
-    self.transferStack = proxy.transferStack
+    if not controllerSide then
+        for i = 0, 5 do
+            self.name = proxy.getInventoryName(i)
+            if self.name then
+                controllerSide = i
+                break
+            end
+        end
+    end
+    self.name = proxy.getInventoryName(controllerSide)
+    if not self.name then
+        error("No inventory " .. relativeSidePositions[controllerSide] ..
+                  " the given transposer.")
+    end
+    self.size = proxy.getInventorySize(controllerSide)
 
+    function self.compareStacks(slot1, slot2)
+        return proxy.compareStacks(controllerSide, slot1, slot2)
+    end
+    function self.transferItem(otherTransposerSide, itemCount, slot, otherSlot)
+        return proxy.transferItem(controllerSide, otherTransposerSide,
+                                  itemCount, slot, otherSlot)
+    end
+
+    ---@param slot number index 1
+    ---@return itemStack
     function self.getStackInSlot(slot)
         ---@type itemStack
-        local stack = proxy.getStackInSlot(slot)
-        stack.maxStackSize = proxy.getSlotMaxStackSize(slot)
+        local stack = proxy.getStackInSlot(controllerSide, slot)
+        stack.maxStackSize = proxy.getSlotMaxStackSize(controllerSide, slot)
+        return stack
     end
 
     local content = {}
     local function getContent()
-        content = proxy.getAllStacks()
+        content = proxy.getAllStacks(controllerSide)
         for i in ipairs(content) do
-            content[i].maxStackSize = proxy.getSlotMaxStackSize(i)
+            content[i].maxStackSize = proxy.getSlotMaxStackSize(controllerSide,
+                                                                i)
         end
     end
 
@@ -39,5 +72,7 @@ function inventory.new(address, location)
 
     return self
 end
+
+tileEntity.addType(inventory.componentType, inventory)
 
 return inventory
