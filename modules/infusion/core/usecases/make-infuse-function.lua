ea80de0fa1afec437baa97ef94e921b75709a31d
gordominossi <gabriel.minossi@flashapp.com.br>
2022-09-07 03:18:05 -0300

    refactor(make-infuse-function): wait for matrix to be read and for infusion to be done

--- modules/infusion/core/usecases/make-infuse-function.lua
+++ modules/infusion/core/usecases/make-infuse-function.lua
@@ -1,36 +1,87 @@
 local storeRequiredEssentia = require('modules.infusion.core.persistence.store-required-essentia')
 
----comment
----@param recipeToInfuse InfusionRecipe
----@return function
-local function makeInfuseFunction(recipeToInfuse)
-    return function()
-        local altar = recipeToInfuse.altar
-        altar.requestCraft(recipeToInfuse.pattern.outputs[1])
+---Waits for a read from the `altar` and returns the read `essentia`
+---@param altar Altar
+---@return Essentia[] essentia
+local function waitForRead(altar)
+    local essentia = altar.readMatrix()
+    while #essentia == 0 do
+        essentia = altar.readMatrix()
+        coroutine.yield('waiting_on_matrix')
+    end
+    return essentia
+end
 
-        local requiredEssentia = recipeToInfuse.requiredEssentia
-        if not requiredEssentia then
-            altar.blockEssentiaProvider()
+---Waits for the `altar` to have at least the same essentia as the `requiredEssentia`
+---@param altar Altar
+---@param outputName string
+---@param requiredEssentia Essentia[] essentia required for the infusion
+local function waitForEssentia(altar, outputName, requiredEssentia)
+    local missingEssentia = requiredEssentia - altar.getStoredEssentia()
+    -- TODO: find a way to cancel dangling essentia crafts on the ME system CPUs
+    -- altar.requestEssentia(missingEssentia)
+    while #missingEssentia > 0 do
+        coroutine.yield('Missing essentia to craft "' .. outputName .. '": ', missingEssentia)
+        missingEssentia = requiredEssentia - altar.getStoredEssentia()
+    end
+end
+
+local function waitForPedestalItem(altar)
+    local pedestalItem = altar.getPedestalItem()
+    while not pedestalItem do
+        pedestalItem = altar.getPedestalItem()
+    end
+    return pedestalItem
+end
+
+---Waits for the center pedestal item on a particular `altar` to change
+---@param altar Altar
+---@param outputName string
+---@param previousPedestalItem ItemStack
+local function waitForInfusion(altar, outputName, previousPedestalItem)
+    local pedestalItemLabel = (altar.getPedestalItem() or {}).label
+    while previousPedestalItem.label == pedestalItemLabel do
+        coroutine.yield('Remaining essentia to infuse "' .. outputName .. '": ', altar.readMatrix())
+        pedestalItemLabel = (altar.getPedestalItem() or {}).label
+    end
+    if pedestalItemLabel.label ~= outputName then
+        print('Infused item "' .. tostring(pedestalItemLabel) .. 'does not match pattern output "' .. outputName .. '".')
+    end
+end
+
+---Makes the function that controls the infusion of a particular `recipe`
+---@param recipe InfusionRecipe
+---@return function
+local function makeInfuseFunction(recipe)
+    local altar = recipe.altar
+    local firstPatternOutput = recipe.pattern.outputs[1]
+    local requiredEssentia = recipe.requiredEssentia
+
+    return function()
+        local previousPedestalItem
+        if requiredEssentia and (#requiredEssentia > 0) then
+            waitForEssentia(altar, firstPatternOutput.name, requiredEssentia)
+
+            altar.unblockEssentiaProvider()
+
+            altar.requestCraft(firstPatternOutput)
             altar.activateMatrix()
-            local essentia = altar.readMatrix()
-            storeRequiredEssentia(recipeToInfuse.pattern, essentia)
+            previousPedestalItem = waitForPedestalItem(altar)
+            waitForRead(altar)
+        else
+            altar.blockEssentiaProvider()
+
+            altar.requestCraft(firstPatternOutput)
+            previousPedestalItem = waitForPedestalItem(altar)
+            altar.activateMatrix()
+            local essentia = waitForRead(altar)
+            storeRequiredEssentia(recipe.pattern, essentia)
+
             altar.unblockEssentiaProvider()
         end
 
-        local missingEssentia = altar.getStoredEssentia() - requiredEssentia
-        altar.requestEssentia(missingEssentia)
-        while missingEssentia do
-            missingEssentia = altar.getStoredEssentia() - requiredEssentia
-            coroutine.yield("waiting_on_essentia", missingEssentia)
-        end
-
-        altar.activateMatrix()
-
-        while altar.isBusy() do
-            coroutine.yield("waiting_on_infusion", altar.readMatrix())
-        end
-
-        altar.retireveCraftedItem()
+        waitForInfusion(altar, firstPatternOutput.name, previousPedestalItem)
+        altar.retrieveCraftedItem(firstPatternOutput.count)
     end
 end
 
