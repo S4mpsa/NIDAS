ee6c32987849bf5ddcffe0191590a05f24d3bbbd
gordominossi <gabriel.minossi@flashapp.com.br>
2022-09-07 03:00:23 -0300

    fix(get-recipe-to-infuse): store `known recipes to reduce memory usage

--- modules/infusion/core/usecases/get-recipe-to-infuse.lua
+++ modules/infusion/core/usecases/get-recipe-to-infuse.lua
@@ -1,13 +1,26 @@
 local getRequiredEssentia = require('modules.infusion.core.persistence.get-required-essentia')
 local findPatterns = require('modules.infusion.core.usecases.find-patterns')
 
-local function getRecipeToInfuse(knownAltars)
-    local pattern, altar = findPatterns(knownAltars)
-    if not pattern then
+
+local knownRecipes = {}
+---Returns the next available recipe to be infused
+---@return InfusionRecipe recipeToInfuse
+local function getRecipeToInfuse()
+    local pattern, altar = findPatterns()
+    if not pattern or not pattern.outputs or not pattern.outputs[1] or not pattern.outputs[1].name then
         return nil
     end
-    local requiredEssentia = getRequiredEssentia(pattern)
-    return { altar = altar, pattern = pattern, requiredEssentia = requiredEssentia }
+
+    if not knownRecipes[altar.id .. pattern.outputs[1].name] then
+        local requiredEssentia = getRequiredEssentia(pattern)
+        knownRecipes[altar.id .. pattern.outputs[1].name] = {
+            altar = altar,
+            pattern = pattern,
+            requiredEssentia = requiredEssentia,
+        }
+    end
+
+    return knownRecipes[altar.id .. pattern.outputs[1].name]
 end
 
 return getRecipeToInfuse
