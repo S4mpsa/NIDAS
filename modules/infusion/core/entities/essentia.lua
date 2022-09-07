cc0443835948fac4bf5c64cc2384c942c5fddf07
gordominossi <gabriel.minossi@flashapp.com.br>
2022-09-07 13:11:35 -0300

    test(essentia): add essentia tests

--- modules/infusion/core/entities/essentia.lua
+++ modules/infusion/core/entities/essentia.lua
@@ -1,12 +1,12 @@
 ---@class Essentia
 ---@field amount number
----@field name string
+---@field label string
 local Essentia = {}
 
 local function tostring(essentiaListTable)
     local essentiaString = ''
     for _, essentia in ipairs(essentiaListTable) do
-        essentiaString = essentiaString .. '\n' .. essentia.name .. ': ' .. essentia.amount
+        essentiaString = essentiaString .. '\n' .. essentia.label .. ': ' .. essentia.amount
     end
     return essentiaString
 end
@@ -14,9 +14,9 @@ end
 local function subtraction(t1, t2)
     local result = {}
     for _, essentia1 in ipairs(t1) do
-        table.insert(result, { name = essentia1.name, amount = essentia1.amount })
+        table.insert(result, { label = essentia1.label, amount = essentia1.amount })
         for _, essentia2 in ipairs(t2) do
-            if essentia2.name == essentia1.name then
+            if essentia2.label == essentia1.label then
                 local amount = essentia1.amount - essentia2.amount
                 if amount > 0 then
                     result[#result].amount = amount
@@ -38,10 +38,14 @@ function Essentia.new(aspects)
     if type(aspects) ~= "table" then
         aspects = {}
     end
+    local newAspects = {}
     for i, aspect in ipairs(aspects) do
-        aspects[i] = { amount = aspect.amount, name = aspect.label or (aspect.name .. ' Gas') }
+        newAspects[i] = {
+            amount = aspect.amount,
+            label = aspect.label or (aspect.name .. ' Gas'),
+        }
     end
-    return setmetatable(aspects, mt)
+    return setmetatable(newAspects, mt)
 end
 
 return Essentia
