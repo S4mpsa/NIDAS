3155fa1bd02771c036058bc91ebfb6ccf823844a
gordominossi <gabriel.minossi@flashapp.com.br>
2022-09-07 03:36:13 -0300

    chore(init): change init to load modules

--- init.lua
+++ init.lua
@@ -1,9 +1,14 @@
 -- Adds NIDAS library folders to default package path
 package.path = package.path .. ";/home/NIDAS/?.lua;/home/NIDAS/lib/?.lua"
 
-local tileEntity = require("core.tile-entity")
-while true do
-    for entity in tileEntity.list() do
-        entity.update()
-    end
-end
+-- local modules = { require("modules.infusion.init") }
+-- while true do
+--     for name, coro in pairs(modules) do
+--         local status, result = coroutine.resume(coro)
+--         if not status then
+--             print("Error on module " .. name .. ": " .. result)
+--         end
+--     end
+-- end
+
+require("modules.infusion.init")
