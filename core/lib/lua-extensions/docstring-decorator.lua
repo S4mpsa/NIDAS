45c4998d6cc412172850349d49c956ccbb69a8b2
gordominossi <gabriel.minossi@flashapp.com.br>
2022-09-22 19:42:47 -0400

    refactor(docstring-decorator): format and document function

--- core/lib/lua-extensions/docstring-decorator.lua
+++ core/lib/lua-extensions/docstring-decorator.lua
@@ -1,4 +1,4 @@
-local Decorator = require("core.lib.lua-extensions.decorator")
+local Decorator = require('core.lib.lua-extensions.decorator')
 
 --- Makes the function.__tostring = description.
 ---
@@ -8,16 +8,21 @@ local Decorator = require("core.lib.lua-extensions.decorator")
 ---     local myPrint =
 ---       docstring([[calls .__tostring on whatever arguments it's given]]) ..
 ---       function(args) print(args) end
----@type decorator (description:string):decorator string: string to describe the function being decorated
-local docstring = Decorator(function(description, functionToDescribe)
-    return setmetatable({}, {
-        __call = function(_, ...)
-            functionToDescribe(...)
-        end,
-        __tostring = function()
-            return description
-        end,
-    })
-end)
+---@type decorator
+local docstring = Decorator(
+    ---@param description string string to describe the function being decorated
+    ---@param functionToDescribe fun()
+    ---@return table describedFunction actually a table with `__call` and `__tostring` metamethods
+    function(description, functionToDescribe)
+        return setmetatable({}, {
+            __call = function(_, ...)
+                functionToDescribe(...)
+            end,
+            __tostring = function()
+                return description
+            end,
+        })
+    end
+)
 
 return docstring
