d0ceb20a7ad026475d0edb5b53ec48f4c84b261e
gordominossi <gabriel.minossi@flashapp.com.br>
2022-11-05 22:57:25 -0300

    refactor(infusion repositories): rename repositories

--- modules/infusion/core/repositories/altar-repository.lua
+++ modules/infusion/core/repositories/altar-repository.lua
@@ -1,21 +1,24 @@
 local component = require('component')
 local Altar = require('modules.infusion.core.aggregates.altar')
 local Matrix = require('modules.infusion.core.entities.matrix')
-local MeInterface = require('modules.infusion.core.entities.me-interface')
-local RedstoneIO = require('core.tile-entity.redstone-io')
-local transposer = require('core.tile-entity.transposer')
+local MeInterface = require('core.tile-entities.me-interface')
+local RedstoneIO = require('core.tile-entities.redstone-io')
+local Transposer = require('core.tile-entities.transposer')
 
+local AltarRepository = {}
 local altar = Altar.new(
     component[RedstoneIO.entityType].address,
     component[RedstoneIO.entityType].address,
     component[Matrix.entityType].address,
     component[MeInterface.entityType].address,
-    component[transposer.entityType].address
+    component[Transposer.entityType].address
 )
-local function getKnownAltars()
+
+---@return Altar[]
+function AltarRepository.getAll()
     local altars = { altar }
 
     return altars
 end
 
-return getKnownAltars
+return AltarRepository
--- modules/infusion/core/repositories/infusion-recipe-repository.lua
+++ modules/infusion/core/repositories/infusion-recipe-repository.lua
@@ -4,12 +4,14 @@ local Essentia = require('modules.infusion.core.dtos.essentia')
 
 local dataDir = '/home/NIDAS/data/'
 filesystem.makeDirectory(dataDir)
+local filePath = dataDir .. 'required-essentia.data'
+
+local InfusionRecipeRepository = {}
 
 ---Gets the persisted required essentia for a given pattern
 ---@param pattern Pattern
 ---@return Essentia[]
-local function getRequiredEssentia(pattern)
-    local filePath = dataDir .. 'required-essentia.data'
+function InfusionRecipeRepository.findByPattern(pattern)
     local file
     if filesystem.exists(filePath) then
         file = io.open(filePath, 'r')
@@ -22,24 +24,22 @@ local function getRequiredEssentia(pattern)
     ) or {}
     file:close()
 
+    ---@diagnostic disable-next-line: return-type-mismatch
     return Essentia.new(knownRequiredEssentia[pattern.outputs[1].name])
 end
 
 ---Persists the required essentia for a given pattern
----@param pattern Pattern
----@param essentia Essentia
-local function storeRequiredEssentia(pattern, essentia)
-    local file = io.open(dataDir .. 'required-essentia.data', 'w')
+---@param recipe InfusionRecipe
+function InfusionRecipeRepository.upsert(recipe)
+    local file = io.open(filePath, 'w')
     local knownRequiredEssentia = serialization.unserialize(
         file:read('*a') or '{}'
     ) or {}
-    knownRequiredEssentia[pattern.outputs[1].name] = essentia
+    knownRequiredEssentia[recipe.pattern.outputs[1].name]
+        = recipe.requiredEssentia
 
     file:write(serialization.serialize(knownRequiredEssentia))
     file:close()
 end
 
-return {
-    getRequiredEssentia = getRequiredEssentia,
-    storeRequiredEssentia = storeRequiredEssentia,
-}
+return InfusionRecipeRepository
